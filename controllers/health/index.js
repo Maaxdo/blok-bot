aa2e57c415955fdcfb0a8fa33a40da4a8afa88ad
Mfoniso <mfonisoischris@gmail.com>
2025-08-02 09:30:08 +0100

    fixes

--- controllers/health/index.js
+++ controllers/health/index.js
@@ -1,13 +1,26 @@
-const { successResponse } = require("../../helpers/common/httpResponse");
-
-const healthCheckerController = (req, res) => {
-  return successResponse({
-    res,
-    data: {
-      status: "UP",
-    },
-    message: "Health check passed",
-  });
+const {
+  successResponse,
+  failedResponse,
+} = require("../../helpers/common/httpResponse");
+const { sequelize } = require("../../db/models");
+const healthCheckerController = async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    return successResponse({
+      res,
+      data: {
+        status: "UP",
+      },
+      message: "Health check passed",
+    });
+  } catch (error) {
+    return failedResponse({
+      res,
+      message: error.message,
+      status: 500,
+      err: error,
+    });
+  }
 };
 
 module.exports = { healthCheckerController };
