52c0d99d3e79877c1d094036d6c48a13c3f6a48b
mfoniso <mfonisoischris@gmail.com>
2025-10-08 13:06:39 +0100

    fixes: bug fixes

--- utils/webhook/commands/wallet.js
+++ utils/webhook/commands/wallet.js
@@ -31,7 +31,7 @@ async function handleAssets(user, message) {
   const walletsInfo = wallets
     .map(
       (wallet) =>
-        `Wallet type 💲: *${wallet.wallet_type}*\nBalance 💰: *${wallet.balance}*\nAddress 📄: ${wallet.address}\nNetwork: ${wallet.network}`,
+        `Wallet type 💲: *${wallet.wallet_type}*\nBalance 💰: *${wallet.balance}*\nAddress 📄: ${wallet.address}\nNetwork 📄: ${wallet.network}`,
     )
     .join("\n\n");
 
