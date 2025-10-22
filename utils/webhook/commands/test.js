72f1c5661e15011ee4414274e289c2e8883e4a8c
mfoniso <mfonisoischris@gmail.com>
2025-10-22 11:30:15 +0100

    test

--- utils/webhook/commands/test.js
+++ utils/webhook/commands/test.js
@@ -1,4 +1,22 @@
-async function test(user, message) {}
+const { sendFlow } = require("../../../helpers/bot/infobip");
+
+async function test(user, message) {
+  await sendFlow({
+    user,
+    text: "Submit KYC details\n🚀 Get your KYC done in 2 minutes.\nYou will need the following:\n\n- BVN\nClick the message to write in your bvn\n\nForgotten your BVN? Dial *565.0# to retrieve your BVN",
+    action: {
+      mode: "PUBLISHED",
+      flowMessageVersion: 3,
+      flowToken: "Flow token",
+      flowId: "1294142805561038",
+      callToActionButton: "Continue",
+      flowAction: "NAVIGATE",
+      flowActionPayload: {
+        screen: "WELCOME_SCREEN",
+      },
+    },
+  });
+}
 
 module.exports = {
   test,
