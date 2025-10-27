e034e47940fdbec20dad7d4995a66c0d1e3520e5
mfoniso <mfonisoischris@gmail.com>
2025-10-27 16:12:38 +0100

    feat: test

--- utils/webhook/commands/test.js
+++ utils/webhook/commands/test.js
@@ -3,16 +3,16 @@ const { sendFlow } = require("../../../helpers/bot/infobip");
 async function test(user, message) {
   await sendFlow({
     user,
-    text: "Submit KYC details\n🚀 Get your KYC done in 2 minutes.\nYou will need the following:\n\n- BVN\nClick the message to write in your bvn\n\nForgotten your BVN? Dial *565.0# to retrieve your BVN",
+    text: 'Your wallet is ready! 🎉 Now, let\'s secure it with a PIN.\nClick on "CONTINUE" to create your 4-digit transaction PIN. 🔒\nThis keeps your wallet safe and only accessible to you. 🔒',
     action: {
       mode: "PUBLISHED",
       flowMessageVersion: 3,
       flowToken: "Flow token",
-      flowId: "1294142805561038",
+      flowId: "1467934914360314",
       callToActionButton: "Continue",
       flowAction: "NAVIGATE",
       flowActionPayload: {
-        screen: "WELCOME_SCREEN",
+        screen: "PIN_SCREEN",
       },
     },
   });
